5c0e25b3e37589dfc2fb960a050e12b4f4872df1
Jose Sut <jsut914@gmail.com>
2023-12-05 11:16:20 -0600

    Elaborando la función validarUsuario del login

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 import cerrarVentanaLogin from "./login/cerrarLogin.js";
 import iniciarSecion from "./login/iniciarSecion.js";
+import validarUsuario from "./login/validarUsuario.js";
+
+const usuarios = [{
+    usuario: "admin01",
+    password: "admin01"
+},{
+    usuario: "admin02",
+    password: "admin02"
+}
+];
 
 iniciarSecion();
 cerrarVentanaLogin();
+validarUsuario(usuarios);
---
+++ js/login/validarUsuario.js
@@ -0,0 +1,25 @@
+const validarUsuario = (listaUsuarios)=>{
+    const formularioLogin = document.querySelector('[data-formularioLogin]');
+    const spanError = document.getElementsByClassName('inicioSecion__password--error')[0];
+
+    formularioLogin.addEventListener('submit',(e)=>{
+        e.preventDefault();
+        const valorUsuario = e.target.querySelector('[data-usuarioLogin]').value;
+        const valorPassword = e.target.querySelector('[data-inputPassword]').value;
+
+        const acceso = listaUsuarios.find((datosUsuario) => {
+            return datosUsuario.usuario === valorUsuario && datosUsuario.password === valorPassword;
+        });
+
+        if(acceso){
+            spanError.classList.add('passwordError');
+            window.location.href = '/assets/pages/home/homeAmin.html';
+        }else{
+            console.log("Contraseña incorrecta");
+            spanError.classList.add('inicioSecion__password--error');
+            spanError.classList.remove('passwordError');
+        }
+    });
+};
+
+export default validarUsuario;
